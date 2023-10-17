aec81bb6f82332c18ac4a1a313663cb3c8efef28
Ruicong Wang <ruicong.wang@student.kuleuven.be>
2023-10-17 10:05:48 +0200

    update EmergencyCenter.h

--- headers/EmergencyCenter.h
+++ headers/EmergencyCenter.h
@@ -1,24 +1,35 @@
 #ifndef EMERGENCYCENTER_H
 #define EMERGENCYCENTER_H
 
-#include <iostream>
 #include <string>
 #include <vector>
 
-#include "Sensor.h"
+// Forward declaration of the Component class to use as a pointer.
+class Component;
 
-class EmergencyCenter
-{
-protected:
-    //Singleton so the constructor should be private or protected
-    EmergencyCenter();
-    static EmergencyCenter* ec_instance;
+class EmergencyCenter {
+private:
+    static EmergencyCenter* instance; // Singleton instance
+    std::vector<Component*> components;
 
 public:
-    static EmergencyCenter* GetInstance();
+    // Constructor & Destructor
+    EmergencyCenter();
+    ~EmergencyCenter();
 
-private:
-
+    // Singleton related methods
+    static EmergencyCenter* getInstance();
+    
+    // Member functions as per the UML diagram
+    void addComponent(Component* component);
+    void printSensorsOrderedByLocation();
+    void activateAllSensors();
+    void deactivateAllSensors();
+    bool testAllSensors();
+    void activateComponent();
+    void deactivateComponent();
+    void testComponent();
+    void updateAllSoftware();
 };
 
 #endif // EMERGENCYCENTER_H
