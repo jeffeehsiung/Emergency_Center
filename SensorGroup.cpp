228aee584257326b90938c420a45e0e3c8fcf35d
Jeffee Hsiung <chiehfei.hsiung@student.kuleuven.be>
2023-10-24 11:00:41 +0200

    sensorGroup

--- SensorGroup.cpp
+++ SensorGroup.cpp
@@ -1,10 +1,30 @@
 #include "SensorGroup.h"
 
-void SensorGroup::addStrategy(Component *component)
+
+void SensorGroup::addSensor(Component& component)
 {
-    
+    /** the component parameter is a reference to a Sensor component. 
+     * the method adds the component to the vector of components 
+     **/
+    components.push_back(component);
 }
 
-void SensorGroup::removeStrategy(Component *component)
+void SensorGroup::removeSensor(Component& component)
 {
+    /** the component paramter is a reference to a Sensor component.
+     * the method removes the component from the vector of components based on the component's id
+    */
+    for (int i = 0; i < components.size(); i++)
+    {
+        if (components[i].getId() == component.getId())
+        {
+            components.erase(components.begin() + i);
+        }
+    }
+}
+
+void SensorGroup::getSensors()
+{
+    /** return the updated vector of components **/
+    return components;
 }
--- headers/SensorGroup.h
+++ headers/SensorGroup.h
@@ -1,9 +1,11 @@
 #include "Component.h"
+#include <memory> // for std::shared_ptr
 
 class SensorGroup: public Component{
 protected:
-    std::vector<Component*> components;
+    std::vector<Component&> components;
 public:
-    virtual void addStrategy(Component* component);
-    virtual void removeStrategy(Component* component);
+    virtual void addSensor(Component& component);
+    virtual void removeSensor(Component& component);
+    virtual void getSensors();
 };
