081691141e76208fc3fb77fde928ea14af48e439
Ruicong Wang <ruicong.wang@student.kuleuven.be>
2023-10-24 14:10:28 +0200

    add Component.cpp

---
+++ Component.cpp
@@ -0,0 +1,108 @@
+#include "headers/Component.h"
+
+// constructor
+Component::Component(std::string id, std::string location, std::string vendor, int activationTimeStart, int activationTimeEnd, std::list<std::string> monitorScope, bool isActive, bool alwaysActive, int deactivationTime, int softwareVersion)
+    : id{id}, location{location}, vendor{vendor}, activationTimeStart{activationTimeStart}, activationTimeEnd{activationTimeEnd}, monitorScope{monitorScope}, isActive{isActive}, alwaysActive{alwaysActive}, deactivationTime{deactivationTime}, softwareVersion{softwareVersion} {
+    std::cout << "Component constructor 1 called with parameters " << id << ", " << location << ", " << vendor << ", " << activationTimeStart << ", " << activationTimeEnd << ", " << monitorScope << ", " << isActive << ", " << alwaysActive << ", " << deactivationTime << ", " << softwareVersion << std::endl;
+}
+
+Component::~Component() {}
+
+std::string Component::getId() const {
+    return id;
+}
+
+void Component::setId(const std::string& id) {
+    this->id = id;
+}
+
+std::string Component::getLocation() const{
+    return location;
+}
+void Component::setLocation(const std::string& location){
+    this->location = location;
+}
+
+std::string Component::getVendor() const{
+    return vendor;
+}
+void Component::setVendor(const std::string& vendor){
+    this->vendor = vendor;
+}
+
+int Component::getActivationTimeStart() const{
+    return activationTimeStart;
+}
+int Component::getActivationTimeEnd() const{
+    return activationTimeEnd;
+}
+void Component::setActivationTime(int start, int end){
+    this->activationTimeStart = start;
+    this->activationTimeEnd = end;
+}
+
+std::list<std::string> Component::getMonitorScope() const{
+    return monitorScope;
+}
+void Component::setMonitorScope(const std::list<std::string>& scope){
+    this->monitorScope = scope;
+}
+
+bool Component::getIsActive() const{
+    return isActive;
+}
+void Component::setActive(){
+    this->isActive = true;
+}
+void Component::setNotActive(){
+    this->isActive = false;
+}
+
+bool Component::getAlwaysActive() const{
+    return alwaysActive;
+}
+void Component::setAlwaysActive(bool alwaysActiveStatus){
+    this->alwaysActive = alwaysActiveStatus;
+}
+
+int Component::getDeactivationTime() const{
+    return deactivationTime;
+}
+void Component::setDeactivationTime(int deactivation){
+    this->deactivationTime = deactivation;
+}
+void Component::updateSoftware(){
+    this->softwareVersion++;
+}
+int Component::getSoftwareVersion() const{
+    return softwareVersion;
+}
+
+std::ostream& operator<<(std::ostream& COUT, const std::list<std::string>& list) {
+    COUT << "[";
+    for (const auto& item : list) {
+        COUT << item;
+        if (&item != &list.back()) COUT << ", ";
+    }
+    COUT << "]";
+    return COUT;
+}
+
+std::ostream& operator<<(std::ostream& COUT, const Component& component) {
+    // Print component details using its getter methods
+    COUT << "ID: " << component.getId() << "\n"
+         << "Location: " << component.getLocation() << "\n"
+         // ... Continue this pattern for all member variables ...
+         << "Deactivation time: " << component.getDeactivationTime() << "\n\n\n";
+    return COUT;
+}
+
+Component& Component::operator++() {
+    this->setActive();
+    return *this;
+}
+
+Component& Component::operator--() {
+    this->setNotActive();
+    return *this;
+}
